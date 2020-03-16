e07138a4c7c0ef4903efc739b98cc9e22039f2c8
henok keraga <henokkeraga@gmail.com>
2020-03-16 16:21:01 -0500

    lab-3 docker

--- lab_3/greetingservice/src/main/java/edu/miu/greetingservice/model/Book.java
+++ lab_3/greetingservice/src/main/java/edu/miu/greetingservice/model/Book.java
@@ -1,17 +1,6 @@
 package edu.miu.greetingservice.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Document(collection = "book")
 public class Book {
     @Id
@@ -20,7 +9,45 @@ public class Book {
     private double price ;
     private String title;
 
+    public String getIsbn() {
+        return isbn;
+    }
 
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
--- lab_3/greetingservice/src/main/java/edu/miu/greetingservice/resource/BookController.java
+++ lab_3/greetingservice/src/main/java/edu/miu/greetingservice/resource/BookController.java
@@ -3,18 +3,26 @@ package edu.miu.greetingservice.resource;
 import edu.miu.greetingservice.model.Book;
 import edu.miu.greetingservice.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Component
 @RestController
 public class BookController {
 
     @Autowired
     private BookService bookService;
+    @GetMapping("/")
+    public String start(){
+        return "test";
+    }
+
 
     @GetMapping("/books")
     public ResponseEntity<?> getAllBooks(){
